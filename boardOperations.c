a5e2ba0afd90a4e8fe8e46d92fcace7965338fb8
jembaba58 <viking.jem@gmail.com>
2017-04-16 17:07:55 +0100

    Update boardOperations.c

--- boardOperations.c
+++ boardOperations.c
@@ -11,9 +11,9 @@
 #include <time.h>
 #include "crossfireOperations.h"
 
-void createBoard(int boardSize, struct slot **upLeft, struct slot **upRight, struct slot **downLeft, struct slot **downRight, struct slot **board)
+void createBoard(int boardSize, struct slot **board)
 {
-	int i, j;
+	int i, j, k, t, d;
 
 	for(i=0; i<boardSize; i++)
 	{
@@ -26,12 +26,8 @@ void createBoard(int boardSize, struct slot **upLeft, struct slot **upRight, str
 		}
 	}
 
-
-
-
-
-	for(i=1; i<boardSize-1; i++){
-		for(j=1;j<boardSize-1; j++){
+	for(i=1; i< boardSize-1; i++){
+		for(j=1;j < boardSize-1; j++){
 			board[i][j].up = &board[i-1][j];
 			board[i][j].right = &board[i][j+1];
 			board[i][j].down = &board[i+1][j];
@@ -39,58 +35,57 @@ void createBoard(int boardSize, struct slot **upLeft, struct slot **upRight, str
 		}
 	}
 
-	for(int j=1; j<boardSize-1; j++){
+	for(j = 1; j < boardSize -1; j++){
 		board[0][j].right = &board[0][j+1];
 		board[0][j].left = &board[0][j-1];
 		board[0][j].down = &board[1][j];
+
 		board[boardSize - 1][j].right = &board[boardSize - 1][j+1];
 		board[boardSize -1][j].left = &board[boardSize - 1][j-1];
 		board[boardSize - 1][j].up = &board[boardSize - 2][j];
 	}
 
-	for(int i=1; i<boardSize-1; i++){
+	for(i = 1; i < boardSize -1; i++){
 		board[i][0].right = &board[i][1];
 		board[i][0].up = &board[i-1][0];
 		board[i][0].down = &board[i+1][0];
-		board[i][boardSize-1].left = &board[boardSize - 1][boardSize-2];
+
+		board[i][boardSize-1].left = &board[boardSize-1][boardSize-2];
 		board[i][boardSize -1].up = &board[i -1][boardSize-1];
 		board[i][boardSize -1].down = &board[i+1][boardSize -1];
 	}
 
 	board[0][0].right = &board[0][1];
 	board[0][0].down = &board[1][0];
+
 	board[0][boardSize-1].left = &board[0][boardSize-2];
 	board[0][boardSize -1].down = &board[1][boardSize -1];
+
 	board[boardSize -1][0].right = &board[boardSize -1][1];
 	board[boardSize -1][0].up = &board[boardSize -2][0];
+
 	board[boardSize - 1][boardSize-1].up = &board[boardSize-2][boardSize-1];
 	board[boardSize - 1][boardSize -1].left = &board[boardSize -1][boardSize -2];
 
-	*upLeft = &board[0][0];
-	*upRight = &board[0][boardSize -1];
-	*downLeft = &board[boardSize -1][0];
-	*downRight = &board[boardSize -1][boardSize -1];
-
-	int d;
-	for(i=0; i < boardSize; i++)
+	for(k=0; k < boardSize; k++)
 	{
-		for(d=0; d < boardSize; i++)
+		for(d=0; d < boardSize; d++)
 		{
-			j = 1 + rand() % 3; //j selects a random position between 1 and 3.
+			t = 1 + rand() % 3; //j selects a random position between 1 and 3.
 
-			if(j==1)
+			if(t==1)
 			{
-				strcpy(board[i][d].type, "Level Ground");
+				strcpy(board[k][d].type, "Level Ground");
 			}
 
-			else if(j==2)
+			else if(t==2)
 			{
-				strcpy(board[i][d].type, "Hill");
+				strcpy(board[k][d].type, "Hill");
 			}
 
 			else
 			{
-				strcpy(board[i][d].type, "City");
+				strcpy(board[k][d].type, "City");
 			}
 		}
 	}
